4f86a4b3a91008391b6716b05653b9ef8df27405
talamahmoud <tala.ismail.kafa@gmail.com>
2024-05-08 23:46:01 +0300

    Crud Department without Update

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,40 @@
+﻿using DepartmentCrud.Data;
+using DepartmentCrud.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DepartmentCrud.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        public IActionResult Index()
+        {
+            var departments = context.Departments.ToList();
+            return View("Index",departments);
+        }
+        public IActionResult Details(int id)
+        {
+            var department = context.Departments.First(department =>department.Id == id );
+            return View("Details",department);
+        }
+        public IActionResult Create() 
+        {
+            return View("Create");
+        }
+
+        public IActionResult Store(Department department)
+        {
+           context.Departments.Add(department);
+            context.SaveChanges();
+            return RedirectToAction("Index");   
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var department = context.Departments.Find(id);
+            context.Departments.Remove(department);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DepartmentCrud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DepartmentCrud.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=ENG-TALA-ISMAIL;database=DepartmentCrud;trusted_connection=true;TrustServerCertificate=True;");
+        }
+    }
+}
